23aaa5f1e7ce5e33f1fbf1c188f90d9e12e4dc1e
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2025-03-10 20:55:23 +0200

    add quizzes 7 and 8

--- src/data/quizzes.ts
+++ src/data/quizzes.ts
@@ -2,6 +2,7 @@ import lec1 from './lec1.json';
 import lec2 from './lec2.json';
 import lec3 from './lec3.json';
 import lec4 from './lec4.json';
+import lec5 from './lec5.json';
 
 export const quizzes = [
   {
@@ -27,5 +28,11 @@ export const quizzes = [
     title: "Μάθημα 4",
     description: "",
     questions: lec4
+  },
+  {
+    id: "quiz-5",
+    title: "Μάθημα 5",
+    description: "",
+    questions: lec5
   }
 ];
